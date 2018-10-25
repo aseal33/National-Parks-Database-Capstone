2ec1c5de9746419ec92ad4ea29db4f5f630f76cd
Adam Seal <aseal33@gmail.com>
2018-10-25 08:34:17 -0400

    Progress made towards scheduling query

---
+++ CapstonePractice.sql
@@ -0,0 +1,63 @@
+SELECT *
+FROM site
+LEFT JOIN reservation ON site.site_id = reservation.site_id
+INNER JOIN campground ON site.campground_id = campground.campground_id
+WHERE site.campground_id = 1 
+AND (((to_date NOT BETWEEN '20181022' AND '20181031')
+AND (from_date NOT BETWEEN ('20181022') AND '20181031'))
+OR reservation.site_id IS NULL);
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT *
+FROM  ( SELECT *
+			FROM reservation
+			INNER JOIN [site] ON [site].site_id = reservation.site_id
+			INNER JOIN campground ON [site].campground_id = campground.campground_id
+		  ) AS a
+WHERE campground_id = 1 AND ((to_date NOT BETWEEN '20181022' AND '20181031')
+AND (from_date NOT BETWEEN ('20181022') AND '20181031'))
+OR site_id IS NULL;
+
+
+
+
+SELECT [site].site_id
+FROM site
+INNER JOIN campground ON campground.campground_id = site.campground_id
+LEFT JOIN ( SELECT *
+			FROM reservation
+			WHERE (to_date BETWEEN '20181022' AND '20181031')
+		AND (from_date BETWEEN '20181022' AND '20181031')) AS sub
+		ON reservation.site_id = site.site_id
+WHERE site.campground_id = 1;
+
+
+
+
+
+
+
+
+
+SELECT site_id
+FROM site
+INNER JOIN reservation ON site.site_id = reservation.site_id
+INNER JOIN campground ON site.campground_id = campground.campground_id
+INNER JOIN park ON campground.park_id = park.park_id
+WHERE (
+SELECT 
+FROM 
+WHERE campground.park_id = 1 
+AND (((to_date NOT BETWEEN '20181022' AND '20181031')
+AND (from_date NOT BETWEEN ('20181022') AND '20181031'))
+OR reservation.site_id IS NULL));
