bd17047805001a7977c1189ae84d733ab7a83df8
Jessica Cray-Smith <jsmith005@defiance.edu>
2018-10-26 14:25:05 -0400

    Worked on Campground Availability

--- Capstone.Tests/CampgroundTests.cs
+++ Capstone.Tests/CampgroundTests.cs
@@ -38,5 +38,36 @@ namespace Capstone.Tests
             Assert.AreEqual(1, campgrounds.Count);
 
         }
+        [TestMethod]
+        public void GetAvailability_ReturnsCount()
+        {
+            // Arrange
+            CampgroundSqlDAL dal = new CampgroundSqlDAL(ConnectionString);
+
+            int iD = 0;
+            DateTime start = new DateTime(0000, 00, 00);
+            DateTime end = new DateTime(0000, 00, 00);
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand($"SELECT * FROM site INNER JOIN campground ON site.campground_id = campground.campground_id INNER JOIN reservation ON site.site_id = reservation.site_id WHERE from_date = '2018-06-30' AND to_date = '2018-07-21'", conn);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    iD = Convert.ToInt32(reader["park_id"]);
+                    start = Convert.ToDateTime(reader["from_date"]);
+                    end = Convert.ToDateTime(reader["to_date"]);
+                    
+                }
+            }
+
+            // Act
+            // Campground test = new Campground();
+            IList<Campsite> campsite = dal.CampgroundAvailability(iD, start, end);
+
+
+            // Assert
+            Assert.AreEqual(1, campsite.Count);
+        }
     }
 }
