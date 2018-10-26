cabc4522be9ea4734a75553cdc90a96da28ff107
Jessica Cray-Smith <jsmith005@defiance.edu>
2018-10-26 10:14:58 -0400

    Added initial test data into database.sql file and entered in our superclass test

--- Capstone.Tests/ParkDB_Tests.cs
+++ Capstone.Tests/ParkDB_Tests.cs
@@ -1,13 +1,54 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Transactions;
 
 namespace Capstone.Tests
 {
     [TestClass]
     public class ParkDB_Tests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public const string ConnectionString = @"Data Source=.\SQLEXPRESS; Initial Catalog=Capstone; Integrated Security=True";
+        TransactionScope transaction;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            // BEGIN TRANSACTION
+            transaction = new TransactionScope();
+
+            // Read SQL from database.sql
+            string sql = File.ReadAllText("database.sql");
+
+            // Execute SQL against live database
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            // ROLLBACK TRANSACTION
+            transaction.Dispose();
+
+            // COMMIT TRANSACTION
+            // transaction.Complete();
+        }
+
+        public int GetRowCount(string table)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT COUNT(*) FROM {table}", conn);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                return count;
+            }
         }
     }
 }
--- Capstone.Tests/database.sql
+++ Capstone.Tests/database.sql
@@ -1,1 +1,38 @@
-﻿
+﻿DELETE FROM campground;
+DELETE FROM campsite;
+DELETE FROM park;
+DELETE FROM reservation;
+
+
+-- INSERT Fake Data  ----------------------------
+
+----- PARK ----------------------------------
+INSERT INTO park (name, location, establish_date, area, visitors, description)
+	VALUES ('The Greatest Park in the World', 'Cedar Point', '1953-07-15', 40000, 250000, 'This is the greatest roller coaster park in America. Seriously, if you think there is a better one.. you are sadly mistaken!');
+DECLARE @park int = (SELECT @@IDENTITY);
+
+
+----- CAMPGROUND --------------------------------
+INSERT INTO campground (park_id, name, open_from, open_to, daily_fee)
+	VALUES (@park, 'Crystal Lake', 1, 12, 30.00);
+DECLARE @campground int = (SELECT @@IDENTITY);
+--DECLARE @dpt int;
+--SET @dpt = (SELECT department_id FROM department);
+
+
+----- CAMPSITE -----------------------------------
+INSERT INTO site (campground_id, site_number, max_occupancy, accessible, max_rv_length, utilities)
+	VALUES (@campground, 1, 6, 0, 0, 1);
+DECLARE @campsite int = (SELECT @@IDENTITY);
+-- DECLARE @proj int;
+-- SET @proj = (SELECT project_id FROM project);
+
+
+----- RESERVATION ---------------------------
+INSERT INTO reservation (campsite_id, name, from_date, to_date, create_date)
+	VALUES (@campsite, 'Seal', '2018-7-22', '2018-7-29', GETDATE());
+DECLARE @reservation int = (SELECT @@IDENTITY);
+
+
+----- Make all of these variables available: ----
+SELECT @park AS park, @campground AS campground, @campsite AS campsite, @reservation AS reservation;
